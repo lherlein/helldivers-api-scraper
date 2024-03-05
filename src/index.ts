13823e8221f9c0e3c2fab8233b1d8d23eb8e9386
Luca Herlein <luca@herlein.com>
2024-03-04 19:36:05 -0800

    added info endpoints, no filtering

--- src/index.ts
+++ src/index.ts
@@ -19,56 +19,148 @@ import express from 'express';
 const app = express();
 const port: number = 13131;
 
-let rawInfoData: MiddlemanGetResponse;
-let rawStatusData: MiddlemanGetResponse;
+let rawInfoData: RawInfo;
+let rawStatusData: RawStatus;
 
-async function grabData() {  
-  rawInfoData = await hdGet(infoUrl);
-  console.log(`Grabbed first set of info data at ${rawInfoData.timestamp}`);
-  rawStatusData = await hdGet(statusUrl);
-  console.log(`Grabbed first set of status data at ${rawStatusData.timestamp}`);
+let infobase = '/info';
+let statusbase = '/status';
 
-  // make middleman request every checkInterval minutes to limit requests to helldivers API
-  let checkInterval = 10; // in minutes
-  setInterval(async () => {
-    rawInfoData = await hdGet(infoUrl);
-    console.log(`Grabbed new info data at ${rawInfoData.timestamp}`);
-    rawStatusData = await hdGet(statusUrl);
-    console.log(`Grabbed new status data at ${rawStatusData.timestamp}`);
-  }, checkInterval*60*1000);
-}
+const appEndpoints = {
+  info: {
+    warId: `${infobase}/warId`,
+    dates: `${infobase}/dates`,
+    minimumClientVersion: `${infobase}/minimumClientVersion`,
+    planetInfos: `${infobase}/planetInfos`,
+    homeWorlds: `${infobase}/homeWorlds`
+  },
+  status: {
+    warId: `${statusbase}/warId`,
+    time: `${statusbase}/time`,
+    impactMultiplier: `${statusbase}/impactMultiplier`,
+    storyBeatId32: `${statusbase}/storyBeatId32`,
+    planetStatus: `${statusbase}/planetStatus`,
+    planetAttacks: `${statusbase}/planetAttacks`,
+    campaigns: `${statusbase}/campaigns`,
+    communityTargets: `${statusbase}/communityTargets`,
+    jointOperations: `${statusbase}/jointOperations`,
+    planetEvents: `${statusbase}/planetEvents`,
+    planetActiveEffects: `${statusbase}/planetActiveEffects`,
+    activeElectionPolicyEffects: `${statusbase}/activeElectionPolicyEffects`,
+    globalEvents: `${statusbase}/globalEvents`,
+    superEarthWarResults: `${statusbase}/superEarthWarResults`
+  }
+};
 
 function server() {  
   // Define a basic endpoints
-  app.get('/rawinfo', async (req, res) => {
+  app.get('/', async (req, res) => {
+    const reqData: RequestData = {
+      "timestamp": new Date().toISOString(),
+      "request": req
+    };
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
+  });
+  
+  app.get(infobase, async (req, res) => {
     // log request with req info and time
     const reqData: RequestData = {
       "timestamp": new Date().toISOString(),
       "request": req
     };
-    console.log(`Request for rawinfo at ${reqData.timestamp} from ${reqData.request.ip}`);
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
     // Set proper content-type header for JSON
     res.setHeader('Content-Type', 'application/json');
     // Return a JSON object
     res.json(rawInfoData);
   });
 
-  app.get('/rawstatus', async (req, res) => {
+  app.get(statusbase, async (req, res) => {
     const reqData: RequestData = {
       "timestamp": new Date().toISOString(),
       "request": req
     };
-    console.log(`Request for rawinfo at ${reqData.timestamp} from ${reqData.request.ip}`);
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
     // Set proper content-type header for JSON
     res.setHeader('Content-Type', 'application/json');
     // Return a JSON object
     res.json(rawStatusData);
   });
 
+  app.get(appEndpoints.info.warId, async (req, res) => {
+    const reqData: RequestData = {
+      "timestamp": new Date().toISOString(),
+      "request": req
+    };
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
+    res.json(rawInfoData.warId);
+  });
+
+  app.get(appEndpoints.info.dates, async (req, res) => {
+    const reqData: RequestData = {
+      "timestamp": new Date().toISOString(),
+      "request": req
+    };
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
+    
+    let resJson = {
+      startDate: rawInfoData.startDate,
+      endDate: rawInfoData.endDate
+    }
+
+    res.json(resJson);
+  });
+
+  app.get(appEndpoints.info.minimumClientVersion, async (req, res) => {
+    const reqData: RequestData = {
+      "timestamp": new Date().toISOString(),
+      "request": req
+    };
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
+    res.json(rawInfoData.minimumClientVersion);
+  });
+
+  app.get(appEndpoints.info.planetInfos, async (req, res) => {
+    const reqData: RequestData = {
+      "timestamp": new Date().toISOString(),
+      "request": req
+    };
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
+    res.json(rawInfoData.planetInfos);
+  });
+
+  app.get(appEndpoints.info.homeWorlds, async (req, res) => {
+    const reqData: RequestData = {
+      "timestamp": new Date().toISOString(),
+      "request": req
+    };
+    console.log(`${reqData.timestamp}: ${reqData.request.ip} requested ${reqData.request.originalUrl}`);
+    res.json(rawInfoData.homeWorlds);
+  });
+
   // Start the server
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
+}
+
+async function grabData() {  
+  let rawInfo = await hdGet(infoUrl);
+  rawInfoData = rawInfo.data as RawInfo;
+  console.log(`Grabbed first set of info data at ${rawInfo.timestamp}`);
+  let rawStatus = await hdGet(statusUrl);
+  rawStatusData = rawStatus.data as RawStatus;
+  console.log(`Grabbed first set of status data at ${rawStatus.timestamp}`);
+
+  // make middleman request every checkInterval minutes to limit requests to helldivers API
+  let checkInterval = 10; // in minutes
+  setInterval(async () => {
+    let rawInfo = await hdGet(infoUrl);
+    rawInfoData = rawInfo.data as RawInfo;
+    console.log(`Grabbed new info data at ${rawInfo.timestamp}`);
+    let rawStatus = await hdGet(statusUrl);
+    rawStatusData = rawStatus.data as RawStatus;
+    console.log(`Grabbed new status data at ${rawStatus.timestamp}`);
+  }, checkInterval*60*1000);
 }
 
 async function start() {
